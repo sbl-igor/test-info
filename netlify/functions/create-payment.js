2557ef5caa7861dbc0d815dec01ba125384dae22
Sobolev Igor <shokeator98@gmail.com>
2025-02-26 12:34:11 +1000

    payment

--- netlify/functions/create-payment.js
+++ netlify/functions/create-payment.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 const crypto = require("crypto");
 
-const SHOP_ID = '123456';
-const SECRET_KEY = 'test_ABC123';
+const SHOP_ID = process.env.YOOKASSA_SHOP_ID; // Данные хранятся в переменных окружения
+const SECRET_KEY = process.env.YOOKASSA_SECRET_KEY;
 
 exports.handler = async function(event) {
     const idempotenceKey = crypto.randomUUID();
